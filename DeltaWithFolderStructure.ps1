cf631bc083a666d54cda7a2ac526dc53db9d4024
priyanka2596 <40689523+priyanka2596@users.noreply.github.com>
2018-11-06 15:31:01 +0530

    Update DeltaWithFolderStructure.ps1
    changes

--- DeltaWithFolderStructure.ps1
+++ DeltaWithFolderStructure.ps1
@@ -1,13 +1,36 @@
-Param (
-[String]$PrevCommit
-)
-#Get latest commit id from the TFS-git
-$gitLatestCommit=git rev-parse HEAD
-write-host $gitLatestCommit
+$last2Commits = git log -2 --pretty=tformat:%h%n
+write-host "Last 2 commits are : $last2Commits"
 
-
-#Copy file from source to destination
-function Copy-New-Item {
+if($last2Commits)
+{
+	$gitLatestCommit = $last2Commits.split("")[0]
+	write-host "actual Latest commit id : $gitLatestCommit"
+	$prevCommit = $last2Commits.split("")[2]
+	write-host "actual previous commit id : $prevCommit"
+	$myMergeCommitInfo = git show $gitLatestCommit
+	
+	if(($myMergeCommitInfo) -and $myMergeCommitInfo[1].split("")[0] -contains "Merge:")
+	{
+		write-host "Latest commit is merge and the commits inside it are : "$myMergeCommitInfo[1].split("")
+		
+		$newMergeLatestCommit = $myMergeCommitInfo[1].split(" ")[2]
+		write-host "New Merge latest commit id : $newMergeLatestCommit"
+		
+		$newMergeprevCommit = $myMergeCommitInfo[1].split(" ")[1]
+		write-host "New Merge previous commit id : $newMergeprevCommit"
+		
+		git diff --name-only $newMergeLatestCommit $newMergeprevCommit --relative > lastcommitchanges.txt
+	}
+	else
+	{
+		write-host "Latest commit is not a merge commit
+		git diff --name-only $gitLatestCommit $prevCommit --relative > lastcommitchanges.txt
+	}
+	
+	#old code to generate Delta
+	
+	#code file from source to destination
+	function Copy-New-Item {
 	$SourceFilePath =$args[0]
 	$DestinationFilePath =$args[1]
 	
@@ -16,10 +39,6 @@ function Copy-New-Item {
 	}
 	Copy-Item -Verbose -Path $SourceFilePath -Destination $DestinationFilePath -Force
 	}
-
-	#write newly checked-in file names between the commits to a text file
-	git diff --name-only $gitLatestCommit $PrevCommit --relative > lastcommitchanges.txt
-	
 	#source code path from TFS
 	$path = $PSScriptRoot 
 	write-output "`n" | out-file lastcommitchanges.txt -append
@@ -27,24 +46,21 @@ function Copy-New-Item {
 
 	#source files
 	$s1=@()
+	#Delta files
+	$s11=@()
+	#list of checkedin files between the commits
 	$Files =Get-Content -path "$path\lastcommitchanges.txt" #Give the file path which has Github's latest file list
 	write-host $Files
 
 	for($i2=0;$i2 -lt $Files.Length;$i2++)
 	{
 		$s1+=$path+"\"+$Files[$i2].Replace("/","\")    #source path
-		#Add Source (Git master branch) reference base path instead of "C:\CICD\"
-	}
-
-	#Delta files
-	$s11=@()
-	for($i2=0;$i2 -lt $Files.Length;$i2++)
-	{
-		$s11+=$path+"\Delta\"+$Files[$i2].Replace("/","\") #Delta path #Give the Delta drive Path instead of "C:\Devopp\Delta"
+		$s11+=$path+"\Delta\"+$Files[$i2].Replace("/","\") #Delta path
 	}
 	$s2=$s1.Count
 	$s2.GetType()
-	#all the child items Under src
+	
+	#all the child items Under src from source
 	$master=Get-ChildItem $path"\src" -recurse
 	#iterate through the src/*
 	foreach($m in $master)
@@ -61,7 +77,6 @@ function Copy-New-Item {
 			{
 			#write-host "Files do not exist in latest build: $s1[$i2]`n"
 			}
-			
 		}
 		####Copy META File into Delta ####
 		for($i2=0;$i2 -lt $s1.Length;$i2++)
@@ -76,71 +91,74 @@ function Copy-New-Item {
 			else 
 			{
 					#write-host "Files do not exist in latest build: $s1[$i2]" 
+					#write-host "No meta file found for : " $s1[$i2]"
 			}
 		}
-	}
+	
+	
 	#Copy class test and meta files into Delta 
 	#Get the folder name under src 
 	$lastword =(($m.fullname).ToString().split("\"))[-2]
 	#Iterate through classes folder for Test Classes(suffix as Test)
-If( ($lastword -match "classes") )	
-{
-	for($i2=0;$i2 -lt $s1.Length ;$i2++)
+	If( ($lastword -match "classes") )	
 	{
-		if($m.fullname -Contains $s1[$i2].Replace(".cls","")+"Test.cls")
+		for($i2=0;$i2 -lt $s1.Length ;$i2++)
 		{
-			$folder =$m.fullname
-			#testclasss file name (Suffix as Test)
-			$s12=$s11[$i2].Replace(".cls","")+"Test.cls"
-			Copy-New-Item $folder $s12
-	 
+			if($m.fullname -Contains $s1[$i2].Replace(".cls","")+"Test.cls")
+			{
+				$folder =$m.fullname
+				#testclasss file name (Suffix as Test)
+				$s12=$s11[$i2].Replace(".cls","")+"Test.cls"
+				Copy-New-Item $folder $s12
+ 			}
+			else 
+			{
+				#write-host "Files do not exist in latest build: $s1[$i2]"
+			}
 		}
-		else 
+		for($i2=0;$i2 -lt $s1.length; $i2++)
 		{
-			#write-host "Files do not exist in latest build: $s1[$i2]"
+			if($m.fullname -Contains $s1[$i2].Replace(".cls","")+"Test.cls-meta.xml")
+			{
+				$folder=$m.fullname
+				#test class meta file name(suffix as test)
+				$s12=$s11[$i2].Replace(".cls","")+"Test.cls-meta.xml"
+				Copy-New-Item $folder $s12
+			}
+			else
+			{
+				##write-host "Files do not exist in latest build: $s1[$i2]"
+			}
 		}
-}
-for($i2=0;$i2 -lt $s1.length; $i2++)
-{
-	if($m.fullname -Contains $s1[$i2].Replace(".cls","")+"Test.cls-meta.xml")
-	{
-		$folder=$m.fullname
-		#test class meta file name(suffix as test)
-		$s12=$s11[$i2].Replace(".cls"," ")+"Test.cls-meta.xml"
-		Copy-New-Item $folder $s12
-	}
-	else
-	{
-	##write-host "Files do not exist in latest build: $s1[$i2]"
-	}
-}
 
-#Copy Testclass and meta file into Delta(Prefix as Test)
+		#Copy Testclass and meta file into Delta(Prefix as Test)
 
-for($i2=0;$i2 -lt $s1.Length;$i2++)
-{
-	#Get the class file and meta
-	$lastword=($s1[$i2].ToString().split("\"))[-1]
-	$file3=($s1[$i2].tostring()).trimend("$lastword")
-	$file3=$file3+"Test"+$lastword
-	$file33=$file3+"-meta.xml"
-	if($m.fullname -Contains $file3 -or $m.fullname -Contains $file33)
-	{
-		$folder = $m.fullname
-		$s12=$s11[$i2]
-		$lastword1=($s12.ToString().split("\"))[-1]
-		$file31=($s12.tostring()).trimend($lastword1)
-		$file31=$file31+"Test"+$lastword1
-		$file34=$file31+"-meta.xml"
-		if($m.fullname -Contains $file3)
+		
+		for($i2=0;$i2 -lt $s1.Length;$i2++)
 		{
-			Copy-New-Item $folder $file31
-		}
-		if($m.fullname -Contains $file33)
+		#Get the class file and meta
+		$lastword=($s1[$i2].ToString().split("\"))[-1]
+		$file3=($s1[$i2].tostring()).trimend("$lastword")
+		$file3=$file3+"Test"+$lastword
+		$file33=$file3+"-meta.xml"
+		if($m.fullname -Contains $file3 -or $m.fullname -Contains $file33)
 		{
-			Copy-New-Item $folder $file34
+			$folder = $m.fullname
+			$s12=$s11[$i2]
+			$lastword1=($s12.ToString().split("\"))[-1]
+			$file31=($s12.tostring()).trimend($lastword1)
+			$file31=$file31+"Test"+$lastword1
+			$file34=$file31+"-meta.xml"
+			if($m.fullname -Contains $file3)
+			{
+				Copy-New-Item $folder $file31
+			}
+			if($m.fullname -Contains $file33)
+			{
+				Copy-New-Item $folder $file34
+			}
 		}
 	}
-}
-}
+	}
+	}
 }
